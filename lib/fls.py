1774d78fb7cd0d1bda214deee56b7d45f7e02ecf
embylat <79987979+embylat@users.noreply.github.com>
2021-09-19 19:24:42 -0500

    Create fls.py

---
+++ lib/fls.py
@@ -0,0 +1,14 @@
+def descarch ():
+  ! wget -q -c -nc -O /tmp/1-pls-gdrv.zip https://www.dropbox.com/s/uo5cjdhkyo1vp1n/1-pls-gdrv.zip
+  ! wget -q -c -nc -O /tmp/1-pls-tgrm.zip https://www.dropbox.com/s/t8q5j5pmvbe8i3o/1-pls-tgrm.zip
+  ! wget -q -c -nc -O /tmp/1-srs-gdrv.zip https://www.dropbox.com/s/v4tqot8avyj04el/1-srs-gdrv.zip
+  ! wget -q -c -nc -O /tmp/1-srs-tgrm.zip https://www.dropbox.com/s/xed2n3im1uqozcf/1-srs-tgrm.zip
+  ! wget -q -c -nc -O /tmp/1-pls-yts.zip https://www.dropbox.com/s/owhtwpg29hmj6o5/1-pls-yts.zip
+  ! wget -q -c -nc -O /tmp/1-pls-psa.zip https://www.dropbox.com/s/k2m63t5iit0azg0/1-pls-psa.zip
+  ! wget -q -c -nc https://www.dropbox.com/s/xv4mp78h4cq20be/3mby-cfg.tar.gz
+  ! unzip -qq -o /tmp/1-pls-gdrv.zip -d /tmp/pls-grdv/
+  ! unzip -qq -o /tmp/1-pls-tgrm.zip -d /tmp/pls-tgrm/
+  ! unzip -qq -o /tmp/1-pls-yts.zip -d /tmp/pls-yts/
+  ! unzip -qq -o /tmp/1-pls-psa.zip -d /tmp/pls-psa/
+  ! unzip -qq -o /tmp/1-srs-gdrv.zip -d /tmp/srs-grdv/
+  ! unzip -qq -o /tmp/1-srs-tgrm.zip -d /tmp/srs-tgrm/
